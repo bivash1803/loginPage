42cb8bcbd86d5b5406dae3b67ecea51c2a2aa6f2
Bivash <bivashbaidya32@gmail.com>
2023-05-13 13:22:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from "react";
+ import * as Components from './Components';
+
+ function App() {
+     const [signIn, toggle] = React.useState(true);
+      return(
+          <Components.Container>
+              <Components.SignUpContainer signinIn={signIn}>
+                  <Components.Form>
+                      <Components.Title>Create Account</Components.Title>
+                      <Components.Input type='text' placeholder=' Enter Name' />
+                      <Components.Input type='email' placeholder=' Enter Email' />
+                      <Components.Input type='password' placeholder=' Enter Password' />
+                      <Components.Button>Sign Up</Components.Button>
+                  </Components.Form>
+              </Components.SignUpContainer>
+
+              <Components.SignInContainer signinIn={signIn}>
+                   <Components.Form>
+                       <Components.Title>Sign in</Components.Title>
+                       <Components.Input type='email' placeholder=' Enter Email' />
+                       <Components.Input type='password' placeholder='  Enter Password' />
+                       <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
+                       <Components.Button>Sigin In</Components.Button>
+                   </Components.Form>
+              </Components.SignInContainer>
+
+              <Components.OverlayContainer signinIn={signIn}>
+                  <Components.Overlay signinIn={signIn}>
+
+                  <Components.LeftOverlayPanel signinIn={signIn}>
+                      <Components.Title>Welcome To My Site!</Components.Title>
+                      <Components.Paragraph>
+                           Please login here
+                      </Components.Paragraph>
+                      <Components.GhostButton onClick={() => toggle(true)}>
+                          Sign In
+                      </Components.GhostButton>
+                      </Components.LeftOverlayPanel>
+
+                      <Components.RightOverlayPanel signinIn={signIn}>
+                        <Components.Title>Welcome User!</Components.Title>
+                        <Components.Paragraph>
+                            Enter Your details and start journey with Perimattic Pvt Ltd
+                        </Components.Paragraph>
+                            <Components.GhostButton onClick={() => toggle(false)}>
+                                Sigin Up
+                            </Components.GhostButton> 
+                      </Components.RightOverlayPanel>
+  
+                  </Components.Overlay>
+              </Components.OverlayContainer>
+
+          </Components.Container>
+      )
+ }
+
+ export default App;
